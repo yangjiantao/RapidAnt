1eae002071e201adde97a231c5ec8266b8039cf5
jiantaoyang <jiantaoyoung@gmail.com>
2019-02-02 15:37:53 +0800

    feat: add cp dbFile method

--- app/src/main/java/io/jiantao/rapidant/MainActivity.java
+++ app/src/main/java/io/jiantao/rapidant/MainActivity.java
@@ -2,9 +2,14 @@ package io.jiantao.rapidant;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.jiantao.utils.java.FileUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void wanAndroid(View view){
         startActivity(new Intent(this, io.jiantao.example.wanandroid.MainActivity.class));
+    }
+
+    public void wanAndroiddb(View view){
+        final String dbName = "rapid_ant.db";
+        File dbFile = getDatabasePath(dbName).getParentFile();
+        File destFile = new File(Environment.getExternalStorageDirectory(), "rapidAntDbDir");
+        boolean state = FileUtils.copyDir(dbFile, destFile);
+        Toast.makeText(this, "拷贝数据库 "+(state?"成功":"失败"), Toast.LENGTH_SHORT).show();
     }
 }
